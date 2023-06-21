3a069e743e4016a8df7dc58ede3513d55061b886
copaiement <cole.paiement@gmail.com>
2023-06-21 16:01:26 -0700

    Updated Computer move code

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -2,9 +2,32 @@ import gameboard from './gameboard';
 import ship from './ship';
 
 const player = () => {
-  // given hits array and misses array, find next shot
-  function shootNextTo(prevShots, misses) {
+  function shootNextTo(sunkHits, hits, misses) {
+    // find the unique hits that have not sunk ships
+    let uniqueHits = [];
+    hits.forEach((hit) => {
+      if (!sunkHits.includes(hit)) uniqueHits.push(hit);
+    });
+    // if there is only one unique hit, just randomly shoot next to it
+    if (uniqueHits.length === 1) {
+      let move = randOffset(uniqueHits[0]);
+      while (hits.includes(move) || misses.includes(move)) {
+        move = randOffset(uniqueHits[0]);
+      }
+      return move;
+    } else {
+
+    }
+  }
+
+  function randOffset(start) {
+    const randOffsets = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+    const offset = randOffsets[Math.floor(Math.random() * randOffsets.length)];
+    let x = parseInt(start.charAt(0), 10) + offset[0];
+    let y = parseInt(start.charAt(1), 10) + offset[1];
     
+    if (x < 0 || x > 9 || y < 0 || y > 9) return randOffset(start); 
+    return `${x}${y}`;
   }
 
   function getRandCoord() {
@@ -39,7 +62,7 @@ const player = () => {
 
     // if there are hits not in sunk ships, shoot close
     if (hits.length > sunkHits.length) {
-
+      gameboard.receiveAttack('p2', shootNextTo(sunkHits, hits, misses));
     } else {
     // else pick a random spot and shoot at it
       gameboard.receiveAttack('p2', shootNew(hits, misses));
