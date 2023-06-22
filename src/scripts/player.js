95dc055c594cd0c58cada29baea22b9572ae33bd
copaiement <cole.paiement@gmail.com>
2023-06-22 08:46:02 -0700

    Update computerTurn with logic

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -4,7 +4,7 @@ import ship from './ship';
 const player = () => {
   function shootNextTo(sunkHits, hits, misses) {
     // find the unique hits that have not sunk ships
-    let uniqueHits = [];
+    const uniqueHits = [];
     hits.forEach((hit) => {
       if (!sunkHits.includes(hit)) uniqueHits.push(hit);
     });
@@ -18,9 +18,47 @@ const player = () => {
     }
     // otherwise shoot along the current line
     // test first two entries of uniqueHits for direction
+    const directionArr = [];
+    let direction;
+    let fixed;
     if (uniqueHits[0].charAt(0) === uniqueHits[1].charAt(0)) {
-
+      uniqueHits.forEach((hit) => directionArr.push(parseInt(hit.charAt(1), 10)));
+      direction = 'y';
+      fixed = uniqueHits[0].charAt(0);
+    } else {
+      uniqueHits.forEach((hit) => directionArr.push(parseInt(hit.charAt(0), 10)));
+      direction = 'x';
+      fixed = uniqueHits[0].charAt(1);
     }
+
+    let move = linearOffset(directionArr, direction, fixed);
+    while ((hits.includes(move) && !uniqueHits.includes(move)) || misses.inclues(move)) {
+      move = linearOffset(directionArr, direction, fixed);
+    }
+    return move;
+  }
+
+  function linearOffset(directionArr, direction, fixed) {
+    // find min and max of directionArr
+    const maxMin = [Math.max(...directionArr), Math.min(...directionArr)];
+    let nextLinear;
+    // if max = 9, use min plus offset
+    if (maxMin[0] >= 9) {
+      nextLinear = maxMin[1] - 1;
+    // if min = 0, use max plus offset
+    } else if (maxMin[1] <= 0) {
+      nextLinear = maxMin[0] + 1;
+    // if no bounds, pick random from max and min
+    } else {
+      const rand = Math.floor(Math.random() * maxMin.length);
+      nextLinear = maxMin[rand];
+    }
+
+    if (direction === 'x') {
+      return `${nextLinear}${fixed}`;
+    }
+
+    return `${fixed}${nextLinear}`;
   }
 
   function randOffset(start) {
@@ -49,16 +87,12 @@ const player = () => {
 
   function computerTurn() {
     // get all previous plays
-    let hits = gameboard.p1Hits;
-    let misses = gameboard.p1Misses;
-    let sunk = gameboard.p1Sunk;
+    const hits = gameboard.p1Hits;
+    const misses = gameboard.p1Misses;
+    const sunk = gameboard.p1Sunk;
 
-    // if there are hits that are not part of sunk ship arrays,
-    // shoot again next to those
-    // if two shots in a row, keep following the row
-    
     // create array of hits on sunk ships
-    let sunkHits = [];
+    const sunkHits = [];
     sunk.forEach((sunkShip) => {
       sunkShip.array.forEach((hit) => sunkHits.push(hit));
     });
@@ -71,6 +105,9 @@ const player = () => {
       gameboard.receiveAttack('p2', shootNew(hits, misses));
     }
   }
-}
+  return {
+    computerTurn,
+  };
+};
 
 export default player;
