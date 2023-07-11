32df3bc02115788ed0cb0a49ec376b26c055817a
copaiement <cole.paiement@gmail.com>
2023-07-11 09:07:40 -0700

    Add more domFunctions, working on eventlisteners

--- src/scripts/domFunctions.js
+++ src/scripts/domFunctions.js
@@ -1,6 +1,9 @@
 // DOM functions
 
 const domFunctions = () => {
+    // track cells with functioning event listeners
+    const listenCells = [];
+    
     function buildGameboards() {
         const playerBoard = document.querySelector('.playerBoard');
         for (let y = 0; y <= 9; y++) {
@@ -21,9 +24,20 @@ const domFunctions = () => {
                 square.classList.add('square');
                 square.classList.add('empty');
                 square.id = `c${x}${y}`;
+                listenCells.push(`c${x}${y}`);
                 computerBoard.appendChild(square);
             }
         }
+    }
+
+    // add event listener to cell
+    function addBoardListener() {
+
+    }
+
+    // remove event listener from cell
+    function removeBoardListener(cellID) {
+        cellID.removeEventListener();
     }
     
     function displayShip(shipsArray) {
@@ -48,6 +62,9 @@ const domFunctions = () => {
         }
 
         cellID = document.getElementById(`${modifier}${attack[1]}`);
+        // remove cell event listener if computer board
+        if (modifier === c) removeBoardListener(cellID);
+
         // update cell based on attack value
         if (attack[0] === 'hit') {
             cellID.classList.remove('ship');
@@ -65,6 +82,29 @@ const domFunctions = () => {
     function animateSinking() {
 
     }
+
+    // *********
+    // REFERENCE animation from KNIGHTS project
+    function moveKnight(e) {
+        removeKnightListeners();
+        const finish = idToArr(e.target.id);
+        const results = knightMoves(knightPos, finish);
+        displayResults(results);
+        const path = results[3];
+        const delay = 1000;
+        let promise = Promise.resolve();
+        path.forEach((move) => {
+          promise = promise.then(() => {
+            updateKnightPos(knightPos, move);
+            return new Promise((resolve) => {
+              setTimeout(resolve, delay);
+            });
+          });
+        });
+        promise.then(() => {
+          addKnightListeners();
+        });
+      }
 
     // update visible list of ships with new sunk ships
     function updateShipList(target, shipType) {
